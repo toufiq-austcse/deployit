55b86388f5db41a33931f9406011f2c780f65eb6
toufiq <toufiq@10minuteschool.com>
2023-01-21 00:39:26 +0600

    Complete dockerization

--- Dockerfile
+++ Dockerfile
@@ -28,14 +28,14 @@ RUN yarn build
 # use one of the smallest images possible
 FROM node:16-alpine
 RUN npm install -g typeorm
-# get package.json from base
-COPY --from=base /app/package.json ./
-# get the dist back
-COPY --from=dev /app/dist/ ./dist/
 # get the node_modules from the intial cache
 COPY --from=base /app/node_modules/ ./node_modules/
 RUN apk add git
 RUN apk add docker
+# get package.json from base
+COPY --from=base /app/package.json ./
+# get the dist back
+COPY --from=dev /app/dist/ ./dist/
 # expose application port
 EXPOSE 3000
 # start
--- migrations/1674217410321-table_creation.ts
+++ migrations/1674217410321-table_creation.ts
@@ -1,4 +1,5 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
+import { DEPLOYMENT_TYPE_STATUS } from '@common/utils/constants';
 
 export class tableCreation1674217410321 implements MigrationInterface {
     name = 'tableCreation1674217410321'
@@ -9,6 +10,7 @@ export class tableCreation1674217410321 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "deployments" ("createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "id" SERIAL NOT NULL, "short_id" character varying NOT NULL, "deployment_type_id" integer NOT NULL, "name" character varying NOT NULL, "sub_domain_name" character varying NOT NULL, "mapped_port" character varying, "status" character varying NOT NULL, "last_deployed_at" TIMESTAMP, "repository_link" text NOT NULL, "branch_name" character varying NOT NULL, "root_dir" character varying, "docker_img_tag" character varying, "container_id" character varying, "user_id" integer NOT NULL, CONSTRAINT "UQ_dfeeaf15886d4a5cd9492546f8f" UNIQUE ("short_id"), CONSTRAINT "PK_1e5627acb3c950deb83fe98fc48" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "environment_variables" ("createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "id" SERIAL NOT NULL, "deployment_id" integer NOT NULL, "key" text NOT NULL, "value" text, CONSTRAINT "PK_58b382642e67cefe198cd9f660b" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "deployments" ADD CONSTRAINT "FK_4a7b4dafb4ceeb52ea417936c49" FOREIGN KEY ("deployment_type_id") REFERENCES "deployment_types"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`INSERT INTO "deployment_types" (id, name, status) VALUES (1, 'web_service', '${DEPLOYMENT_TYPE_STATUS.ENABLED}')`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
