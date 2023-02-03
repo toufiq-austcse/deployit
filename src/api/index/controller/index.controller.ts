a9e08b207980af7ff9b15a3982b875c00d351c8f
toufiq <toufiq@10minuteschool.com>
2023-02-03 17:17:28 +0600

    Log Req Obj

--- src/api/index/controller/index.controller.ts
+++ src/api/index/controller/index.controller.ts
@@ -8,7 +8,14 @@ export class IndexController {
 
   @Get()
   index(@Req() req: Request) {
-    console.log(req);
+    console.log('host name ', req.hostname);
+    console.log('host base url ', req.baseUrl);
+    console.log('host url ', req.url);
+    console.log('host original url ', req.originalUrl);
+    console.log('host path ', req.path);
+    console.log('host protocol ', req.protocol);
+    console.log('host secure ', req.secure);
+    console.log('host ip ', req.ip);
     return {
       app: 'DeployIt is running.....'
     };
