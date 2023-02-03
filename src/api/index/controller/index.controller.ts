9646e3b8090cac05843f5b7b488cda2aa19ece6d
toufiq <toufiq@10minuteschool.com>
2023-02-03 17:11:05 +0600

    Log Req Obj

--- src/api/index/controller/index.controller.ts
+++ src/api/index/controller/index.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
 
 @ApiTags('Index')
 @Controller()
 export class IndexController {
 
   @Get()
-  index() {
+  index(@Req() req: Request) {
+    console.log(req);
     return {
       app: 'DeployIt is running.....'
     };
