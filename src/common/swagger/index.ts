f4553cc35be020c2c3d07fdb2c023910f0a23980
toufiq <toufiq@10minuteschool.com>
2023-02-18 16:38:40 +0600

    -mchange swagger base url

--- src/common/swagger/index.ts
+++ src/common/swagger/index.ts
@@ -5,7 +5,7 @@ import { AppConfigService } from '@common/app-config/service/app-config.service'
 
 export async function setupSwagger(app: INestApplication, port: number) {
 
-  let swaggerDocPath = '/doc';
+  let swaggerDocPath = '/api-doc';
 
   const config = new DocumentBuilder()
     .setTitle(AppConfigService.appConfig.SWAGGER_TITLE)
