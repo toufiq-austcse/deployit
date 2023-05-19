337fa4b4dc0e37bea08edb99b80ccf196f34a871
toufiq <toufiq@10minuteschool.com>
2023-05-19 12:40:40 +0600

    refactor: repo_validate_api: change bad req error format

--- src/api/deployments/dto/res/repository-res.dto.ts
+++ src/api/deployments/dto/res/repository-res.dto.ts
@@ -4,10 +4,6 @@ import { Expose } from 'class-transformer';
 export class ValidateRepositoryResDto {
   @ApiProperty()
   @Expose()
-  is_valid: boolean;
-
-  @ApiProperty()
-  @Expose()
   repo_full_name: string;
 
 }
