0901adcad17bf854d7f076cc23e80913cb74f5ae
toufiq <toufiq@10minuteschool.com>
2023-05-19 17:05:20 +0600

    refactor: repo_validate_api: change bad req error format

--- src/api/deployments/dto/req/repository-req.dto.ts
+++ src/api/deployments/dto/req/repository-req.dto.ts
@@ -4,8 +4,23 @@ import { IsValidGitUrl } from '@common/validators/index';
 
 export class ValidateRepositoryQueryDto {
   @ApiProperty()
-  @IsUrl()
-  @IsNotEmpty()
-  @Validate(IsValidGitUrl)
+  @IsUrl({}, {
+    message: JSON.stringify({
+      key: 'repository_url',
+      message: 'repository_url should be a valid url'
+    })
+  })
+  @IsNotEmpty({
+    message: JSON.stringify({
+      key: 'repository_url',
+      message: 'repository_url is required'
+    })
+  })
+  @Validate(IsValidGitUrl, {
+    message: JSON.stringify({
+      key: 'repository_url',
+      message: 'repository_url should be a valid git url'
+    })
+  })
   repository_url: string;
 }
